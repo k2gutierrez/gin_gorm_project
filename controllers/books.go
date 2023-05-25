6b8b46a1bd8ef872106a38c0a213127362f4f279
k2gutierrez <cchimal.gutierrez@gmail.com>
2023-05-25 16:47:36 -0600

    update function

--- controllers/books.go
+++ controllers/books.go
@@ -12,6 +12,11 @@ type CreateBookInput struct {
 	Author string `json:"author" binding:"required"`
 }
 
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
 // GET /books
 // Get all the books
 func FindBooks(c *gin.Context) {
@@ -47,6 +52,28 @@ func FindBook(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
 		return
 	}
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+// PATCH /books/:id
+// Update a book
+func UpdateBook(c *gin.Context) {
+	//GET model if exist
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	//Validate input
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	models.DB.Model(&book).Updates(input)
 
 	c.JSON(http.StatusOK, gin.H{"data": book})
 }
