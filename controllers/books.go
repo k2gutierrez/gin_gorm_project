d8d5645e6077eb10ba23e1e69931e9e464872021
k2gutierrez <cchimal.gutierrez@gmail.com>
2023-05-25 14:02:16 -0600

    added create book

--- controllers/books.go
+++ controllers/books.go
@@ -7,12 +7,33 @@ import (
 	"github.com/k2gutierrez/gin_gorm_project/models"
 )
 
+type CreateBookInput struct {
+	Title  string `json:"title" binding:"required"`
+	Author string `json:"author" binding:"required"`
+}
+
 // GET /books
 // Get all the books
-
 func FindBooks(c *gin.Context) {
 	var books []models.Book
 	models.DB.Find(&books)
 
 	c.JSON(http.StatusOK, gin.H{"data": books})
+}
+
+// POST /books
+// Create new book
+func CreateBook(c *gin.Context) {
+	//validate input
+	var input CreateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	//create book
+	book := models.Book{Title: input.Title, Author: input.Author}
+	models.DB.Create(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+
 }
